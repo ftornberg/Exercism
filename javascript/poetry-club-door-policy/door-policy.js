10cbb4a631c128e1d91dac7e1b28b0ee05a53382
Fredrik Törnberg <fredrik@webking.se>
2023-03-09 09:42:22 +0100

    wip

--- javascript/poetry-club-door-policy/door-policy.js
+++ javascript/poetry-club-door-policy/door-policy.js
@@ -18,7 +18,7 @@
 // in idiomatic JavaScript, but some companies and style-guides do enforce them.
 //
 // Good luck with that door policy!
-
+let password = ''
 /**
  * Respond with the correct character, given the line of the
  * poem, if this were said at the front door.
@@ -27,7 +27,7 @@
  * @returns {string}
  */
 export function frontDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+	return line.charAt(0)
 }
 
 /**
@@ -38,7 +38,8 @@ export function frontDoorResponse(line) {
  * @returns {string} the front door password
  */
 export function frontDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+	word = word.toLowerCase()
+	return word.charAt(0).toUpperCase() + word.slice(1)
 }
 
 /**
@@ -49,7 +50,7 @@ export function frontDoorPassword(word) {
  * @returns {string}
  */
 export function backDoorResponse(line) {
-  throw new Error('Remove this line and implement the function');
+	return line.charAt(line.length - 1)
 }
 
 /**
@@ -60,5 +61,5 @@ export function backDoorResponse(line) {
  * @returns {string} the back door password
  */
 export function backDoorPassword(word) {
-  throw new Error('Remove this line and implement the function');
+	throw new Error('Remove this line and implement the function')
 }
