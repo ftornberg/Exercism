1e54fb055956a9d5fd6367b9ae896450be31abf9
Fredrik Törnberg <fredrik@webking.se>
2023-03-18 18:53:28 +0100

    mixed juices done

--- javascript/mixed-juices/mixed-juices.js
+++ javascript/mixed-juices/mixed-juices.js
@@ -11,7 +11,20 @@
  * @returns {number} time in minutes
  */
 export function timeToMixJuice(name) {
-  throw new Error('Please implement the timeToMixJuice function');
+	switch (name) {
+		case 'Pure Strawberry Joy':
+			return 0.5
+		case 'Energizer':
+			return 1.5
+		case 'Green Garden':
+			return 1.5
+		case 'Tropical Island':
+			return 3
+		case 'All or Nothing':
+			return 5
+		default:
+			return 2.5
+	}
 }
 
 /**
@@ -23,7 +36,23 @@ export function timeToMixJuice(name) {
  * @returns {number} number of limes cut
  */
 export function limesToCut(wedgesNeeded, limes) {
-  throw new Error('Please implement the limesToCut function');
+	let limesCut = 0
+	let wedges = 0
+	while (wedges < wedgesNeeded && limesCut < limes.length) {
+		switch (limes[limesCut]) {
+			case 'small':
+				wedges += 6
+				break
+			case 'medium':
+				wedges += 8
+				break
+			case 'large':
+				wedges += 10
+				break
+		}
+		limesCut++
+	}
+	return limesCut
 }
 
 /**
@@ -34,5 +63,10 @@ export function limesToCut(wedgesNeeded, limes) {
  * @returns {string[]} remaining orders after the time is up
  */
 export function remainingOrders(timeLeft, orders) {
-  throw new Error('Please implement the remainingOrders function');
+	let i = 0
+	while (timeLeft > 0 && i < orders.length) {
+		timeLeft -= timeToMixJuice(orders[i])
+		i++
+	}
+	return orders.slice(i)
 }
